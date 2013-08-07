9f4e4b0e11f90d9a9d0e15e1b49d28997e9c6bf3
James Kong <fishkingsin@gmail.com>
2013-08-07 12:29:46 +0800

    fixed image size for cocos2d

--- src/com/fishkingsin/holytrickymole/GameCoreActivity.java
+++ src/com/fishkingsin/holytrickymole/GameCoreActivity.java
@@ -1,39 +1,45 @@
 package com.fishkingsin.holytrickymole;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.nio.IntBuffer;
 import java.util.Calendar;
+
+import javax.microedition.khronos.opengles.GL10;
 
 import org.cocos2d.actions.ActionManager;
 import org.cocos2d.actions.base.RepeatForever;
-import org.cocos2d.actions.interval.FadeIn;
-import org.cocos2d.actions.interval.FadeOut;
-import org.cocos2d.actions.interval.JumpTo;
-import org.cocos2d.actions.interval.MoveTo;
-import org.cocos2d.actions.interval.RotateTo;
+import org.cocos2d.actions.interval.IntervalAction;
 import org.cocos2d.actions.interval.Sequence;
+import org.cocos2d.actions.interval.TintBy;
 import org.cocos2d.events.TouchDispatcher;
 import org.cocos2d.layers.ColorLayer;
 import org.cocos2d.layers.Layer;
-import org.cocos2d.nodes.CocosNode;
+import org.cocos2d.menus.MenuItemFont;
+import org.cocos2d.menus.MenuItemImage;
+import org.cocos2d.menus.MenuItemLabel;
 import org.cocos2d.nodes.Director;
-import org.cocos2d.nodes.Label;
+import org.cocos2d.nodes.LabelAtlas;
 import org.cocos2d.nodes.RenderTexture;
 import org.cocos2d.nodes.Scene;
 import org.cocos2d.nodes.Sprite;
 import org.cocos2d.nodes.TextureManager;
 import org.cocos2d.opengl.CCGLSurfaceView;
+import org.cocos2d.types.CCColor3B;
 import org.cocos2d.types.CCColor4B;
-import org.cocos2d.types.CCMacros;
 import org.cocos2d.types.CCPoint;
 import org.cocos2d.types.CCSize;
 
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.CompressFormat;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -48,6 +54,7 @@ import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class GameCoreActivity extends Activity {
 	private static final String LOG_TAG = GameCoreActivity.class
@@ -72,6 +79,7 @@ public class GameCoreActivity extends Activity {
 //				.setPositiveButton("Start", null).show();
 
 		mGLSurfaceView = new CCGLSurfaceView(this);
+		
 		setContentView(mGLSurfaceView);
 	}
 
@@ -186,7 +194,7 @@ public class GameCoreActivity extends Activity {
 					currentView.getHeight());
 			return true;
 		case R.id.saveimage:
-			//mainLayer.saveScreen();
+			saveScreen();
 			return true;
 			
 		case R.id.facebook:
@@ -199,57 +207,101 @@ public class GameCoreActivity extends Activity {
 	}
 	static class MainLayer extends Layer {
 		static final int kTagSprite = 1;
-		Sprite sprite;
+		Sprite sprite;	
 		RenderTexture target;
 		public MainLayer() {
 			CCSize s = Director.sharedDirector().winSize();
-//            target = RenderTexture.renderTexture((int)s.width, (int)s.height);
-//            addChild(target, 1);
+
             isTouchEnabled_ = true;
 			SharedPreferences prefs = PreferenceManager
 					.getDefaultSharedPreferences(mContext);
 			sprite = Sprite.sprite(prefs.getString(mContext.getString(R.string.keyImageName), "grossini.png"));
-			
+			sprite.setScale(s.width/sprite.getWidth());
 			Layer layer = ColorLayer.node(new CCColor4B(93, 113, 112, 255));
 			addChild(layer, -1);
 
 			addChild(sprite, 0, kTagSprite);
-			sprite.setPosition(0, 0);
-//			target = RenderTexture.renderTexture((int)s.width, (int)s.height);
+			sprite.setPosition((int)(s.width*0.5), (int)(s.height*0.5));
+			
+			
+			MenuItemFont.setFontSize(30);
+            MenuItemFont.setFontName("DroidSansMono");
+			 // Image Item
+//            MenuItem item2 = MenuItemImage.item("SendScoreButton.png", "SendScoreButtonPressed.png", this, "menuCallback2");
+//
+//            Menu menu = Menu.menu(item2);
+//            menu.alignItemsVertically();
+//            addChild(menu);
 
-			// sprite.runAction(JumpTo.action(4, 300, 48, 100, 4));
-			//
-			// layer.runAction(RepeatForever.action(Sequence.actions(FadeIn.action(1),
-			// FadeOut.action(1))));
 		}
 
-		// @Override
-		// public boolean ccTouchesBegan(MotionEvent event) {
-		//
-		//
-		// return TouchDispatcher.kEventHandled;
-		// }
+		 @Override
+		 public boolean ccTouchesBegan(MotionEvent event) {
+		
+		
+			 return TouchDispatcher.kEventHandled;
+		 }
 		@Override
 		public boolean ccTouchesMoved(MotionEvent event) {
 			// convert event location to CCPoint
 			CCPoint p = Director.sharedDirector().convertToGL(event.getX(),
 					event.getY());
 
-			// update the position of your sprite
-			sprite.setPosition(p.x, p.y);
 
 			return true;
 
 		}
-		
-		public void saveScreen()
-		{
-			Calendar c = Calendar.getInstance();
-			c.getTimeInMillis();
-			//target.begin();
-//			target.end();
-//			target.saveBuffer(c.toString()+".png");
-		}
+		@Override
+		public boolean ccTouchesEnded(MotionEvent event) {
+	        return TouchDispatcher.kEventIgnored;  // TODO Auto-generated method stub
+	    }
 
+
+	}
+	public static Bitmap SavePixels(int x, int y, int w, int h, GL10 gl)
+	{  
+	     int b[]=new int[w*(y+h)];
+	     int bt[]=new int[w*h];
+	     IntBuffer ib=IntBuffer.wrap(b);
+	     ib.position(0);
+	     gl.glReadPixels(x, 0, w, y+h, GL10.GL_RGBA, GL10.GL_UNSIGNED_BYTE, ib);
+
+	     for(int i=0, k=0; i<h; i++, k++)
+	     {//remember, that OpenGL bitmap is incompatible with Android bitmap
+	      //and so, some correction need.        
+	          for(int j=0; j<w; j++)
+	          {
+	               int pix=b[i*w+j];
+	               int pb=(pix>>16)&0xff;
+	               int pr=(pix<<16)&0x00ff0000;
+	               int pix1=(pix&0xff00ff00) | pr | pb;
+	               bt[(h-k-1)*w+j]=pix1;
+	          }
+	     }
+
+	    Bitmap sb = Bitmap.createBitmap(bt, w, h, Bitmap.Config.ARGB_8888); 
+	    return sb;
+	}
+	public void saveScreen()
+	{
+//		Bitmap bmp = SavePixels(0, 0, 800, 400, Director.sharedDirector().getOpenGLView());
+//
+//        File file = new File("/sdcard/test.jpg");
+//        try
+//        {
+//            file.createNewFile();
+//            FileOutputStream fos = new FileOutputStream(file);
+//            bmp.compress(CompressFormat.JPEG, 100, fos);
+//
+//            Toast.makeText(getApplicationContext(), "Image Saved", 0).show();
+//            Log.i("Menu Save Button", "Image saved as JPEG");
+//        }
+//
+//        catch (Exception e)
+//        {
+//            e.printStackTrace();
+//        }
+
+        
 	}
 }
