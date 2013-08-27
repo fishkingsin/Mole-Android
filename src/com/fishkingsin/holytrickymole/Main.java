680e41bce4926243d124a32f43ca0fcc0e62dd82
James Kong <fishkingsin@gmail.com>
2013-08-27 18:03:24 +0800

    add adMob
    not complete

--- src/com/fishkingsin/holytrickymole/Main.java
+++ src/com/fishkingsin/holytrickymole/Main.java
@@ -1,32 +1,44 @@
 package com.fishkingsin.holytrickymole;
 
+import com.google.ads.Ad;
+import com.google.ads.AdListener;
+import com.google.ads.AdRequest;
+import com.google.ads.AdView;
+import com.google.ads.AdRequest.ErrorCode;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 import android.text.Editable;
 import android.text.Html;
 import android.text.TextWatcher;
 import android.text.method.LinkMovementMethod;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.View.OnClickListener;
 import android.view.View.OnTouchListener;
 import android.view.ViewGroup.LayoutParams;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
-public class Main extends Activity implements OnClickListener {
+public class Main extends FragmentActivity implements OnClickListener {
 	protected static final String TAG = "Main";
 	private Context mContext;
 
@@ -87,6 +99,14 @@ public class Main extends Activity implements OnClickListener {
 			}
 			
 		});
+		
+		if (savedInstanceState == null) {
+			FrameLayout frame = new FrameLayout(this);
+			AdFragment af = AdFragment.newInstance(1);
+            android.app.FragmentTransaction ft = getFragmentManager().beginTransaction();
+            ft.add(frame.getId(),af ).commit();
+        }
+		
 	}
 
 	@Override
@@ -170,4 +190,84 @@ public class Main extends Activity implements OnClickListener {
 			return super.onOptionsItemSelected(item);
 		}
 	}
+	public static class AdFragment extends android.app.Fragment {
+        private int mNum;
+        private AdView mAdView;
+        private TextView mAdStatus;
+
+        static AdFragment newInstance(int num) {
+            AdFragment af = new AdFragment();
+
+            // Supply num input as an argument.
+            Bundle args = new Bundle();
+            args.putInt("num", num);
+            af.setArguments(args);
+
+            return af;
+        }
+
+        /**
+         * When creating, retrieve this instance's number from its arguments.
+         */
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            Bundle args = getArguments();
+            mNum = args != null ? args.getInt("num") : 1;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            // Set up the various ad layouts on different flip pages.
+            final int[] layouts = {
+                    R.layout.ad_top,
+                    R.layout.ad_bottom,
+                    R.layout.ad_next_to_button,
+                    R.layout.ad_covers_content };
+            int layoutId = layouts[mNum];
+            View v = inflater.inflate(layoutId, container, false);
+            mAdStatus = (TextView) v.findViewById(R.id.status);
+            mAdView = (AdView) v.findViewById(R.id.ad);
+            mAdView.setAdListener(new MyAdListener());
+
+            AdRequest adRequest = new AdRequest();
+             adRequest.addKeyword("a1521c74c858d66");
+
+            // Ad network-specific mechanism to enable test mode.  Be sure to disable before
+            // publishing your application.
+            adRequest.addTestDevice("43466EBBCA9A9D93451946825E862160");
+            mAdView.loadAd(adRequest);
+            return v;
+        }
+
+        @Override
+        public void onDestroy() {
+            super.onDestroy();
+            mAdView.destroy();
+        }
+
+        // Receives callbacks on various events related to fetching ads.  In this sample,
+        // the application displays a message on the screen.  A real application may,
+        // for example, fill the ad with a banner promoting a feature.
+        private class MyAdListener implements AdListener {
+
+            @Override
+            public void onDismissScreen(Ad ad) {}
+
+            @Override
+            public void onFailedToReceiveAd(Ad ad, ErrorCode errorCode) {
+                mAdStatus.setText(R.string.error_receive_ad);
+            }
+
+            @Override
+            public void onLeaveApplication(Ad ad) {}
+
+            @Override
+            public void onPresentScreen(Ad ad) {}
+
+            @Override
+            public void onReceiveAd(Ad ad) { mAdStatus.setText(""); }
+        }
+    }
 }
